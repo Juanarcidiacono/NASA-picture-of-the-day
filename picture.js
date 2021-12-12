700d516ae3375e5bcf87feb569db369d21275a79
Juan <juanyarcidiacono@gmail.com>
2021-12-12 17:01:48 -0300

    Picture of the Day

--- picture.js
+++ picture.js
@@ -1,22 +1,20 @@
-function date(){
-  const input = document.getElementById("date")
-  const date = input.value;
+function date() {
 
-  alert(date)
+  const date = document.getElementById("date").value.trim()
   return date
 }
 
-var settings = {
-  "url": "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+date()+"&api_key=DEMO_KEY",
-  "method": "GET",
-  "timeout": 0,
-};
+function settings() {
+  var data = {
+    "url": "https://api.nasa.gov/planetary/apod?date=" + date() + "&api_key=DEMO_KEY",
+    "method": "GET",
+    "timeout": 0,
+  };
 
-$.ajax(settings).done(function (response) {
-  return response
-});
+  $.ajax(data).done(function (response) {
+    var url_picture = response["url"]
+    console.log(url_picture)
 
-
-
-
- 
+    return url_picture;
+  });
+}
